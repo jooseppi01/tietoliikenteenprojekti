69812f8d715462f552c57ccccf2c8c6dd5e7112b
jooseppi01 <t0kajo03@students.oamk.fi>
2022-11-09 13:23:56 +0200

    arduino accelerator luokka valmis

---
+++ arduino/sketch_nov07a/accelerator.h
@@ -0,0 +1,31 @@
+#ifndef ACCELERATOR_H
+#define ACCELERATOR_H
+
+//#include "messaging.h"
+
+
+struct Measurement
+{
+  int x;
+  int y;
+  int z;
+};
+
+
+
+class Accelerator
+{
+public:
+  Accelerator();
+  ~Accelerator();
+  void makeMeasurement();
+  Measurement getMeasurement();
+  void tulostus();
+
+private:
+
+  Measurement m;
+ 
+};
+
+#endif // ACCELERATOR_H
